6712a0ef7dd0d7296004ab1fdf86b93f0b66912c
msardor <sardorm2003@gmail.com>
2023-05-18 23:21:06 +0500

    first commit

---
+++ src/templates/practice/admin/templates.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class PracticeAdminTemplates(Enum):
+    CURRENT_THESISES = "practice/admin/current_thesises_admin.html"
+    CHOOSE_WORKTYPE = "practice/admin/choose_worktype_admin.html"
+    DEADLINE = "practice/admin/deadline_admin.html"
+    THESIS = "practice/admin/thesis_admin.html"
